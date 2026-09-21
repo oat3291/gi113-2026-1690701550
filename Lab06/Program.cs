b0f413831d644a8b10eb8dbcdf72f2fe802c6e17
oat3291 <kaooat1705@gmail.com>
2026-09-21 15:59:00 +0700

    added Lab06

---
+++ Lab06/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Student ID :1690701550
+ * Name       :Sirasak Tumtong
+ * Section    :129B
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+int monsterMaxHp = 100;
+int currentHp = monsterMaxHp;
+Console.WriteLine("==============================================");
+Console.WriteLine($"Monster Attack! [ Monster Hp: {monsterMaxHp}]");
+Console.WriteLine("==============================================");
+Console.WriteLine("");
+Console.Write("Enter Damage To Monster:");
+string input = Console.ReadLine();
+int damage;
+if (int.TryParse(input, out damage))
+{
+    currentHp = currentHp - damage;
+    Console.WriteLine("==============================================");
+    Console.WriteLine($"Your Attack Deal Damage - {damage}"!);
+    Console.WriteLine($"Monster Current Hp Left - {currentHp}"!);
+    Console.WriteLine("==============================================");
+    if (currentHp <= 0)
+    {
+        Console.WriteLine("Monster Die!");
+    }
+    else
+    {
+        Console.WriteLine("Monster Alive!");
+    }
+}
+else
+{
+    Console.WriteLine("Enter Positive Interger Only!");
+}
